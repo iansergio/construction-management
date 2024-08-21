d954a85f04e51e0587064d85611bf89e4db4558b
Ian <iansergiohelf@gmail.com>
2024-08-21 14:41:42 -0300

    feat: CRUD operario adicionado

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import dao.EngenheiroDAO;
+import dao.OperarioDAO;
 import dao.ProjetoDAO;
 import entities.Engenheiro;
+import entities.Operario;
 import entities.Projeto;
 
 public class Main {
@@ -11,6 +13,9 @@ public class Main {
 
         Engenheiro engenheiro = new Engenheiro();
         EngenheiroDAO daoEngenheiro = new EngenheiroDAO();
+
+        Operario operario = new Operario();
+        OperarioDAO daoOperario = new OperarioDAO();
 
         // Inserir projeto
 //        projeto.setNome("");
@@ -36,6 +41,7 @@ public class Main {
             System.out.println(p);
         }
 
+        // ==================
         System.out.println();
 
         // Inserir engenheiro
@@ -58,7 +64,32 @@ public class Main {
             System.out.println(e);
         }
 
+        // ==================
         System.out.println();
 
+        // Inserir Operario
+//        operario.setNome("");
+//        operario.setFuncao("");
+//        daoOperario.inserir(operario);
+
+        // Atualizar operario
+//        operario.setNome("");
+//        operario.setFuncao("");
+//        operario.setId();
+//        daoOperario.atualizar(operario);
+
+        // Excluir operario
+//        operario.setId();
+//        daoOperario.excluir(operario);
+
+        // Listar operario
+        for(Operario o : daoOperario.listar()){
+            System.out.println(o);
+        }
+
+        // ==================
+        System.out.println();
+
+        // Listar equipamento
     }
 }
